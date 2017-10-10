7b904b8b58e2cf083c2574cc29eabff041c49bed
Andy Alt <andy5995@users.noreply.github.com>
2017-10-10 18:01:25 -0500

    updated C app (#30)
    New files generated to https://mhwkb.github.io/

--- mhwkb_md2html.c
+++ mhwkb_md2html.c
@@ -31,24 +31,46 @@
 #include <string.h>
 #include <dirent.h>
 #include <errno.h>
+#include <unistd.h>
 
+#define EXIT_NO_ARGS 2
 #define EXIT_OPENDIR_FAILURE 4
 
-#define VERSION ".0.0.01"
-#define DATE "2017-10-09"
+
+#define VERSION ".0.0.02"
+#define DATE "2017-10-10"
 
 /* Needs lots of error checking added */
+
+void create_html_header (char *html_file);
+void create_html_footer (FILE *html_file);
+
+void erase_char (char *str, char c);
+void trim_char (char *str, char c);
+
+int exists (const char *filename);
 
 int
 main (int argc, char **argv)
 {
-  char link_href[256];
-  char link_title[256];
-  char md_line[512];
+  char *starting_dir = getenv ("PWD");
 
-  int i, href_pos;
+  if (argc == 1)
+  {
+    printf ("Usage: %s <md_file_dir>\n", argv[0]);
+    printf ("  html files will be output to $PWD: %s\n\n", starting_dir);
+    exit (EXIT_NO_ARGS);
+  }
 
-  int status;
+  char index_html[256];
+  strcpy (index_html, starting_dir);
+  strcat (index_html, "/");
+  strcat (index_html, "index.html");
+
+  if (exists (index_html) != 0)
+  {
+    create_html_header (index_html);
+  }
 
   struct dirent *entry;
   DIR *files;
@@ -59,12 +81,33 @@ main (int argc, char **argv)
     exit (EXIT_OPENDIR_FAILURE);
   }
 
+  printf ("%s\n", argv[1]);
+
+  int status;
+  chdir (argv[1]);
   while ((entry = readdir (files)) != NULL)
   {
-    if (strcmp (entry->d_name, ".") == 0 || strcmp (entry->d_name, "..") == 0)
+    /* needs fixing. Only read .md files */
+    if (strcmp (entry->d_name, ".md") == 0)
       continue;
 
     FILE *md_file = fopen (entry->d_name, "r");
+
+    if (md_file == NULL)
+    {
+      perror ("failure: open file\n");
+      exit (1);
+    }
+
+    char link_href[256];
+    char link_title[256];
+    char md_line[512];
+
+    char tags[20][80];
+
+    int i, href_pos;
+
+    char *tag_Ptr = malloc (sizeof (char *));
 
     while (fgets (md_line, 512, md_file) != NULL)
     {
@@ -73,6 +116,16 @@ main (int argc, char **argv)
 
       if (md_line[0] == '[')
       {
+        char tag_html[256];
+
+        FILE *fp_index;
+        fp_index = fopen (index_html, "a");
+
+        if (fp_index == NULL)
+        {
+          perror ("failure: open file\n");
+          exit (1);
+        }
 
         for (i = 0; md_line[i] != ']'; i++)
         {
@@ -94,17 +147,178 @@ main (int argc, char **argv)
             link_href[href_pos++] = md_line[i];
         }
         link_href[href_pos++] = '\0';
-        printf ("<a href=\"%s\">%s</a><br />\n", link_href, link_title);
+        fprintf (fp_index, "<a href=\"%s\">%s</a><br />\n", link_href, link_title);
 
         fgets (md_line, 512, md_file);
-        printf ("date: %s\n", md_line);
+        char *date_line = malloc (sizeof (date_line));
+        strcpy (date_line, md_line);
+        fprintf (fp_index, "date: %s\n", date_line);
+
+        /* get the tags */
         fgets (md_line, 512, md_file);
-        printf ("%s\n", md_line);
-        printf ("<br /><br />");
+
+        tag_Ptr = strtok (md_line, ",");
+
+        int tag_ctr = 0;
+
+        while (tag_Ptr != NULL)
+        {
+          strcpy (tags[tag_ctr], tag_Ptr);
+          /* printf ("%s\n", tags[tag_ctr]); */
+          tag_Ptr = strtok (NULL, ",");
+
+          tag_ctr++;
+        }
+
+        for (i = 0; i < tag_ctr; i++)
+        {
+          while (tags[i][0] != '[' && i == 0)
+          {
+            erase_char (tags[i], tags[i][0]);
+          }
+
+          erase_char (tags[i], '[');
+
+          /* if there's any white space between the [ and the " */
+
+          while (tags[i][0] != '"')
+          {
+            erase_char (tags[i], tags[i][0]);
+          }
+
+          erase_char (tags[i], '"');
+
+          /* check to see if we're on the last tag */
+
+          if (i < tag_ctr - 1)
+          {
+            trim_char (tags[i], '"');
+            strcpy (tag_html, tags[i]);
+            strcat (tag_html, ".html");
+            fprintf (fp_index, "<a href=\"%s\">%s</a>, ", tag_html, tags[i]);
+          }
+          else
+          {
+            int pos = 0;
+
+            while (tags[i][pos] != '"')
+            {
+              pos++;
+            }
+            /* if were' on the last tag, cut off the "]<br />" */
+            tags[i][pos] = '\0';
+            strcpy (tag_html, tags[i]);
+            strcat (tag_html, ".html");
+            fprintf (fp_index, "<a href=\"%s\">%s</a><br /><br />\n", tag_html, tags[i]);
+          }
+        }
+
+        /* Now that we know all the tags for one entry, make the html files
+         */
+
+        for (i = 0; i < tag_ctr; i++)
+        {
+          char html_tag_file[256];
+          strcpy (html_tag_file, starting_dir);
+          strcat (html_tag_file, "/");
+          strcat (html_tag_file, tags[i]);
+          strcat (html_tag_file, ".html");
+
+          if (exists (html_tag_file) != 0)
+          {
+            create_html_header (html_tag_file);
+          }
+
+          FILE *fp;
+
+          fp = fopen (html_tag_file, "a");
+
+          if (fp == NULL)
+          {
+            perror ("failure: open file\n");
+            exit (1);
+          }
+
+          fprintf (fp, "<a href=\"%s\">%s</a><br />\n", link_href, link_title);
+          fprintf (fp, "%s\n", date_line);
+
+          int tag;
+          for (tag = 0; tag < tag_ctr - 1; tag++)
+          {
+            strcpy (tag_html, tags[tag]);
+            strcat (tag_html, ".html");
+            fprintf (fp, "<a href=\"%s\">%s</a>, ", tag_html, tags[tag]);
+          }
+
+          fprintf (fp, "%s<br /><br />", tags[tag]);
+
+          if (fclose (fp) != 0)
+          {
+            perror ("failure: close file\n");
+            exit (1);
+          }
+
+          printf ("\n");
+
+        }
+
+        free (date_line);
+
+        if (fclose (fp_index) != 0)
+        {
+          perror ("failure: close file\n");
+          exit (1);
+        }
+
       }
     }
 
-    fclose (md_file);
+    free (tag_Ptr);
+
+
+    if (fclose (md_file) != 0)
+    {
+      perror ("failure: close file\n");
+      exit (1);
+    }
+
+  }
+
+  status = closedir (files);
+  if (status)
+    perror ("closedir");
+
+  /* struct dirent *entry; */
+
+  files = opendir (starting_dir);
+
+  if (files == NULL)
+  {
+    perror ("opendir");
+    exit (EXIT_OPENDIR_FAILURE);
+  }
+
+  chdir (starting_dir);
+
+  while ((entry = readdir (files)) != NULL)
+  {
+    if ((strcmp (entry->d_name, ".") == 0) || (strcmp (entry->d_name, "..") == 0))
+      continue;
+
+    FILE *fp = fopen (entry->d_name, "a");
+    if (fp == NULL)
+    {
+      perror ("failure: open file\n");
+      exit (1);
+    }
+
+    create_html_footer (fp);
+
+    if (fclose (fp) != 0)
+    {
+      perror ("failure: close file\n");
+      exit (1);
+    }
   }
 
   status = closedir (files);
@@ -112,4 +326,112 @@ main (int argc, char **argv)
     perror ("closedir");
 
   return 0;
+}
+
+
+void create_html_header (char *html_file)
+{
+
+  FILE *fp = fopen (html_file, "w");
+  if (fp == NULL)
+  {
+    perror ("failure: open file\n");
+    exit (1);
+  }
+
+  fprintf (fp, "\
+<!DOCTYPE html>\n\
+<html>\n\
+<head>\n\
+<meta charset=\"UTF-8\">\n");
+  fprintf (fp, "\
+<title>Testing - Mental Health and Wellness Knowledge Base (mhwKB)</title>\n\
+<style>\n\
+p {\n\
+  text-align: left;\n\
+}\n\
+h2 {\n\
+  text-align: center;\n\
+}\n\
+td {\n\
+    text-align: left;\n\
+}\n\
+</style>\n\
+</head>\n\
+<body>\n");
+
+  if (fclose (fp) != 0)
+  {
+    perror ("failure: close file\n");
+    exit (1);
+  }
+
+}
+
+void create_html_footer (FILE *html_file)
+{
+  fprintf (html_file, "\n\
+  <br /><br />\n\
+  <a href=\"index.html\">Back Home</a><br />\n\
+  <a href=\"https://github.com/andy5995/mhwkb\">About</a>\n\
+  </body>\n\
+</html>\n");
+}
+
+/**
+ * Erases characters from the beginning of a string
+ * (i.e. shifts the remaining string to the left
+ */
+void
+erase_char (char *str, char c)
+{
+  int inc = 0;
+
+  while (str[inc] == c)
+    inc++;
+
+  if (!inc)
+    return;
+
+  int n = strlen (str);
+  int i;
+
+  for (i = 0; i < n - inc; i++)
+    str[i] = str[i + inc];
+
+  str[n - inc] = '\0';
+
+  return;
+}
+
+/**
+ * Trim a trailing character if present
+ */
+void
+trim_char (char *str, char c)
+{
+  int len;
+  len = strlen(str) - 1;
+
+  if (str[len] != c)
+    return;
+
+  str[len] = '\0';
+
+  return;
+}
+
+/**
+ * int exists (const char *filename);
+ * Checks for the existence of *filename. On error, uses perror() to
+ * display the reason
+ *
+ * return: the return value of lstat()
+ */
+int
+exists (const char *filename)
+{
+  struct stat st;
+
+  return (lstat (filename, &st));
 }
